2b103719ae2d91832b25e697dea48af5b7b898db
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2024-09-02 05:43:37 +0700

    💄 Add or update the UI and style files.

---
+++ utils/script.js
@@ -0,0 +1,9 @@
+// document.addEventListener("scroll", function () {
+//   const elements = document.querySelectorAll(".slide-transition");
+//   elements.forEach((element) => {
+//     const position = element.getBoundingClientRect();
+//     if (position.top < window.innerHeight && position.bottom > 0) {
+//       element.classList.add("visible");
+//     }
+//   });
+// });
