30d1b0c1a2ff5fea9ef00a668efa2440824ce14d
a6iyyu <ffst4irs@gmail.com>
2024-09-04 21:34:52 +0700

    :boom: Improve structure in home page.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,30 @@
-document.addEventListener("scroll", function () {
-  const elements = document.querySelectorAll(".slide-transition");
-  elements.forEach((element) => {
-    const position = element.getBoundingClientRect();
-    if (position.top < window.innerHeight && position.bottom > 0) {
-      element.classList.add("visible");
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  // Add Scroll Effect On Get Started Button.
+  document.getElementById("get-started").addEventListener("click", () => {
+    document.getElementById("dive-into-the-process").scrollIntoView({
+      behavior: "smooth",
+    });
   });
+
+  // Add Carousel On Home Page
+  new Splide("#karosel-1", {
+    arrows: false,
+    autoplay: true,
+    pagination: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    perPage: 1,
+    snap: true,
+    type: "loop",
+  }).mount();
+
+  // Typing Effect On Home Page
+  const TypedHomeHero = new Typed("#typed-home-hero", {
+    backspeed: 50,
+    loop: true,
+    strings: ["Creativity", "Potential", "Skill"],
+    typeSpeed: 50,
+  });
+
+  return () => TypedHomeHero.destroy();
 });
